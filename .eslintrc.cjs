3d409903a147d78186ad9aa6a4c4f19901d675aa
jofelipe1 <jofelipe@uolinc.com>
2024-03-13 15:10:59 -0300

    chore: improve eslint setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,43 +2,58 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:prettier/recommended",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh", "simple-import-sort", "prettier"],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: ['react-refresh', 'simple-import-sort', 'prettier'],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
+    'react-refresh/only-export-components': [
+      'warn',
       { allowConstantExport: true },
     ],
     // increase the severity of rules so they are auto-fixable
-    "simple-import-sort/imports": "error",
-    "simple-import-sort/exports": "error",
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
   },
   overrides: [
     {
-      files: ["*.js", "*.jsx", "*.ts", "*.tsx"],
+      files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
       rules: {
-        "simple-import-sort/imports": [
-          "error",
+        'simple-import-sort/imports': [
+          'error',
           {
             groups: [
               // Packages `react` related packages come first.
-              ["^react", "^@?\\w"],
+              ['^react', '^@?\\w'],
               // Internal packages.
-              ["^(@|components)(/.*|$)"],
+              ['^(@|components)(/.*|$)'],
               // Side effect imports.
-              ["^\\u0000"],
+              ['^\\u0000'],
               // Parent imports. Put `..` last.
-              ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+              ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
               // Other relative imports. Put same-folder imports and `.` last.
-              ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+              ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
               // Style imports.
-              ["^.+\\.?(css)$"],
+              ['^.+\\.?(css)$'],
             ],
           },
         ],
